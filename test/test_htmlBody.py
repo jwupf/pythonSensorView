83e0ea4ffacfd40cf1690b422c643be871023656
Jörg Wunderlich-Pfeiffer <joerg.wunderlich@gmail.com>
2021-03-01 21:18:54 +0100

    Extract html body tests

---
+++ test/test_htmlBody.py
@@ -0,0 +1,33 @@
+from libs.document.Document import Document
+from libs.document.Chapter import Chapter
+from libs.html.htmlFile import getHtmlBody
+import unittest
+
+
+class TestHtmlBody(unittest.TestCase):
+    def setUp(self):        
+        self.Document = Document()
+        self.DocumentWithChapter = Document()        
+
+        self.FirstChapterTitle = "this is the first chapter title"
+        self.FirstChapterText = "this is text in the first chapter"
+        self.FirstChapter = Chapter(self.FirstChapterTitle)
+        self.DocumentWithChapter.addChapter(self.FirstChapter)
+
+        self.SecondChapterTitle = "this is the second chapter title"
+        self.SecondChapterText = "this is text in the second chapter"
+        self.SecondChapter = Chapter(self.SecondChapterTitle)     
+        self.DocumentWithChapter.addChapter(self.SecondChapter)
+
+
+    def test_htmlPageHasBodyTag(self):
+        page = getHtmlBody(self.Document)
+        self.assertRegex(page, "<body>.*</body>")
+
+    def test_htmlPageHasFirstChapter(self):
+        page = getHtmlBody(self.DocumentWithChapter)
+        self.assertRegex(page, f"<body><h1>{self.FirstChapterTitle}</h1>.*</body>")
+
+    def test_htmlPageHasSecondChapter(self):
+        page = getHtmlBody(self.DocumentWithChapter)
+        self.assertRegex(page, f"<body><h1>.*</h1>.*<h1>{self.SecondChapterTitle}</h1>.*</body>")
--- test/test_htmlFile.py
+++ test/test_htmlFile.py
@@ -9,18 +9,6 @@ class TestHtmlFile(unittest.TestCase):
         self.Title = "this is the page title"
         self.Document = Document(self.Title)
 
-        self.DocumentWithChapter = Document(self.Title)        
-
-        self.FirstChapterTitle = "this is the first chapter title"
-        self.FirstChapterText = "this is text in the first chapter"
-        self.FirstChapter = Chapter(self.FirstChapterTitle)
-        self.DocumentWithChapter.addChapter(self.FirstChapter)
-
-        self.SecondChapterTitle = "this is the second chapter title"
-        self.SecondChapterText = "this is text in the second chapter"
-        self.SecondChapter = Chapter(self.SecondChapterTitle)     
-        self.DocumentWithChapter.addChapter(self.SecondChapter)
-
     def test_htmlPageHasDocType(self):
         page = getHtmlPage(self.Document)
         self.assertRegex(page, "^<!DOCTYPE html>")
@@ -36,14 +24,6 @@ class TestHtmlFile(unittest.TestCase):
     def test_htmlPageHasBodyTag(self):
         page = getHtmlPage(self.Document)
         self.assertRegex(page, ".*</head><body>.*</body>")
-
-    def test_htmlPageHasFirstChapter(self):
-        page = getHtmlPage(self.DocumentWithChapter)
-        self.assertRegex(page, f".*</head><body><h1>{self.FirstChapterTitle}</h1>.*</body>")
-
-    def test_htmlPageHasSecondChapter(self):
-        page = getHtmlPage(self.DocumentWithChapter)
-        self.assertRegex(page, f".*</head><body><h1>.*</h1>.*<h1>{self.SecondChapterTitle}</h1>.*</body>")
 
     def test_htmlPageHasGivenTitle(self):
         head = getHtmlPage(self.Document)
